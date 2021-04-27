5a28ce01ce6a26a97596989c648883c545c31275
Duong Tran <cvinta@gmail.com>
2021-04-27 21:51:20 +0700

    stop at 9. index.tsx <Login/>

--- start/client/src/index_.tsx
+++ start/client/src/index_.tsx
@@ -3,12 +3,14 @@ import {
   NormalizedCacheObject,
   ApolloProvider,
   gql,
+  useQuery,
 } from "@apollo/client";
 import { cache } from "./cache";
 import React from "react";
 import ReactDOM from "react-dom";
 import Pages from "./pages";
 import injectStyles from "./styles";
+import Login from "./pages/login";
 
 export const typeDefs = gql`
   extend type Query {
@@ -27,10 +29,20 @@ const client: ApolloClient<NormalizedCacheObject> = new ApolloClient({
 
 injectStyles();
 
-// Pass the ApolloClient instance to the ApolloProvider component
+const IS_LOGGED_IN = gql`
+  query IsUserLoggedIn {
+    isLoggedIn @client
+  }
+`;
+
+function IsLoggedIn() {
+  const { data } = useQuery(IS_LOGGED_IN);
+  return data.isLoggedIn ? <Pages /> : null;
+}
+
 ReactDOM.render(
   <ApolloProvider client={client}>
-    <Pages />
+    <IsLoggedIn />
   </ApolloProvider>,
   document.getElementById("root")
 );
