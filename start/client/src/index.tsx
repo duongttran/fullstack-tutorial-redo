fdb7efafd552bcf1eb2bbf8f898e2c6351a88b74
Duong Tran <cvinta@gmail.com>
2021-04-27 20:34:08 +0700

    finish 6.Set up Apollo Client - error at npm start final/server

---
+++ start/client/src/index.tsx
@@ -0,0 +1,23 @@
+import { ApolloClient, gql, NormalizedCacheObject } from "@apollo/client";
+import { cache } from "./cache";
+
+const client: ApolloClient<NormalizedCacheObject> = new ApolloClient({
+  cache,
+  uri: "http://localhost:4000/graphql",
+});
+// ...ApolloClient instantiated here...
+
+client
+  .query({
+    query: gql`
+      query TestQuery {
+        launch(id: 56) {
+          id
+          mission {
+            name
+          }
+        }
+      }
+    `,
+  })
+  .then((result) => console.log(result));
