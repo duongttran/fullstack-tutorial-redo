25796c53d244d485a7c0aea99d32fcaa158e8574
Duong Tran <cvinta@gmail.com>
2021-04-27 11:16:16 +0700

    3

--- start/server/src/schema.js
+++ start/server/src/schema.js
@@ -2,11 +2,11 @@ const { gql } = require('apollo-server');
 
 const typeDefs = gql`
   type Launch {
-  id: ID!
-  site: String
-  mission: Mission
-  rocket: Rocket
-  isBooked: Boolean!
+    id: ID!
+    site: String
+    mission: Mission
+    rocket: Rocket
+    isBooked: Boolean!
 }
 
 type Rocket {
